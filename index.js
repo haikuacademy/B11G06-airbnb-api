23a8419168663c2d65b6bf0fd8cf1482cc111e7b
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-15 13:43:14 +0700

    index js added

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express'
 const app = express()
 
+import reviewsRouter from './routes/reviewRoutes.js'
+
+app.use(reviewsRouter)
+
 app.listen(4100, () => console.log('Airbnb API ready on localhost:4100'))
--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -2,13 +2,13 @@ import { Router } from 'express'
 const router = Router()
 
 // Define a GET route for fetching the list of products
-router.get('/reviews', (req, res) => {
-  res.send('List of reviews')
+router.get('/bookings', (req, res) => {
+  res.send('List of bookings')
 })
 
 // Define a GET route for fetching a single user
-router.get('/reviews/1', (req, res) => {
-    res.send({ 'reviewId:1, review: ok'})
+router.get('/booking', (req, res) => {
+  res.send('booking 1')
 })
 
 // Export the router
