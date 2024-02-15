d1c1e2f9acd3f873cd17951ba76dfa3974efbc91
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-15 14:06:30 +0700

    Commiting photos with photos

--- routes/photosroutes.js
+++ routes/photosroutes.js
@@ -3,12 +3,24 @@ const router = Router();
 
 // Define a GET route for fetching the list of users
 router.get("/photos", (req, res) => {
-  res.send("List of photos");
+  res.json([
+    {
+      photo_id: "1",
+      url: "https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      photo_id: "2",
+      url: "https://images.pexels.com/photos/2098405/pexels-photo-2098405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ]);
 });
 
 // Define a GET route for fetching a single user
 router.get("/photos/1", (req, res) => {
-  res.send("Photo number 1");
+  res.send({
+    photo_id: "1",
+    url: "https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  });
 });
 
 // Export the router
