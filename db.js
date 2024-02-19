03be185ea199e2a218d31b9429c3ac4d6e47a890
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-19 10:54:37 +0700

    reviewsRouter

--- db.js
+++ db.js
@@ -1,22 +1,14 @@
 // db.js
 import pg from 'pg'
 const { Pool } = pg
+import { DBURL } from './secrets.js'
 
 // Database connection parameters
 const db = new Pool({
   ssl: {
     rejectUnauthorized: false
   },
-  connectionString:
-    'postgres://phangan_user:Upmwv1wdC9VdWmyaJ6NSTW56uhG8qFW1@dpg-cmsaf87109ks73dteskg-a.singapore-postgres.render.com/phangan'
+  connectionString: DBURL
 })
 
 export default db
-
-import { DBURL } from './secrets.js'
-const db = new Pool({
-  ssl: {
-    rejectUnauthorized: false
-  },
-  connectionString: DBURL
-})
