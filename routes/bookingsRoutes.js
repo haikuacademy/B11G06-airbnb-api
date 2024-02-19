dcdfe5d47e66e9b81d20b7d2954b6513b3b079cc
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-19 11:24:12 +0700

    bookingsRoutes

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js'
 
 // Define a GET route for fetching the list of products
-router.get('/bookings', (req, res) => {
-  res.send('List of bookings')
+router.get('/bookings', async (req, res) => {
+  //remember to async
+  try {
+    const { rows } = await db.query('SELECT * FROM bookings') //database query
+    console.log(rows)
+    res.json(rows) //respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+    res.send('List of bookings')
+  }
 })
 
 // Define a GET route for fetching a single user
