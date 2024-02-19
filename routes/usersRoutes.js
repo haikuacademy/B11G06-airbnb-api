3d04ebb1f44891ebab8934104f52478ec85530d9
therosstar <123678594+therosstar@users.noreply.github.com>
2024-02-19 13:39:46 +0700

    updated users routes queries

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,14 +2,16 @@ import { Router } from 'express'
 const router = Router()
 
 // Define a GET route for fetching the list of users
-router.get('/users', (req, res) => {
-    const listOfUsers = [
-        { id: 1, firstName: 'Alice' },
-        { id: 2, firstName: 'Bob' },
-        { id: 3, firstName: 'Ross' }
-        
-    ]
-  res.json(listOfUsers)
+
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users')
+    console.log(rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 // Define a GET route for fetching a single user
