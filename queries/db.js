bf71c03c74cb0570afb73524c6a7036df74c00ca
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-19 11:18:17 +0700

    created a secrets.js

--- queries/db.js
+++ queries/db.js
@@ -3,11 +3,12 @@ import pg from "pg";
 const { Pool } = pg;
 
 // Database connection parameters
+import { DBURL } from "./secrets.js";
 const db = new Pool({
   ssl: {
     rejectUnauthorized: false,
   },
-  connectionString: "postgres://username:password@host/database",
+  connectionString: DBURL,
 });
 
 export default db;
