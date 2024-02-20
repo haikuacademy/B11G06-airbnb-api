e206cbaef2a68271f76554a166b86a580229e817
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-20 10:55:59 +0700

    fixing up auths

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
 import users from './routes/usersRoutes.js'
 import houses from './routes/housesRoutes.js'
 import reviewsRouter from './routes/reviewsRoutes.js'
 import bookingsRoutes from './routes/bookingsRoutes.js'
 import photosRoutes from './routes/photosRoutes.js'
-import auths from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js'
 
 app.use(reviewsRouter)
 app.use(photosRoutes)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js' // import the database connection
+
+router.get('/auths', async (req, res) => {
+  // don't forget async
+  try {
+    const { rows } = await db.query('SELECT * FROM users') // query the database
+    console.log(rows)
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
 
 // Define a GET route for fetching the list of products
-router.get('/authorizations', (req, res) => {
-  res.json('List of authorizations')
+router.get('/auths', (req, res) => {
+  res.json('List of auths')
 })
 
 // Define a GET route for fetching a single user
