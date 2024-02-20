db63bb37e208d62fe708ca7a645416dd73aa63d3
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-20 11:08:07 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,23 +2,6 @@ import { Router } from 'express'
 const router = Router()
 import db from '../db.js' // import the database connection
 
-router.get('/auths', async (req, res) => {
-  // don't forget async
-  try {
-    const { rows } = await db.query('SELECT * FROM users') // query the database
-    console.log(rows)
-    res.json(rows) // respond with the data
-  } catch (err) {
-    console.error(err.message)
-    res.json(err)
-  }
-})
-
-// Define a GET route for fetching the list of products
-router.get('/auths', (req, res) => {
-  res.json('List of auths')
-})
-
 // Define a GET route for fetching a single user
 router.get('/signup', (req, res) => {
   res.json('signup')
