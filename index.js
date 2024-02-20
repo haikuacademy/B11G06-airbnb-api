a8a8e9d99012166fe4618e073668bba208cf2b54
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-20 10:06:11 +0700

    auth routes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
 const app = express();
 
+// Modules
+
 import reviewsRouter from "./routes/reviewsRoutes.js";
-
 import bookingsRoutes from "./routes/bookingsRoutes.js";
-
-import photosRouter from "./routes/photosRoutes.js";
-
+import photosRouter from "./routes/photosroutes.js";
 import authRouter from "./routes/authRoutes.js";
 
+// Use routes
 app.use(reviewsRouter);
-
 app.use(bookingsRoutes);
-
 app.use(photosRouter);
-
 app.use(authRouter);
 
-app.listen(4100, () => console.log("Airbnb API ready on localhost:4100"));
+// Server
+app.listen(4100, () => {
+  console.log("Airbnb API ready on localhost:4100");
+});
--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -22,6 +22,7 @@ import db from '../db.js' // import the database connection
 //   }
 // })
 
+<<<<<<< Updated upstream
 // // Define a GET route for fetching a single user
 // router.get('/photos/1', (req, res) => {
 //   res.json({
@@ -29,6 +30,15 @@ import db from '../db.js' // import the database connection
 //     url: 'https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
 //   })
 // })
+=======
+// Define a GET route for fetching a single user
+router.get("/photos/1", (req, res) => {
+  res.json({
+    photo_id: "1",
+    url: "https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  });
+});
+>>>>>>> Stashed changes
 
 // router.get('/photos/2', (req, res) => {
 //   res.json({
