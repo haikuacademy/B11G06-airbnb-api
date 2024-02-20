aa2f71fac6e500c864a6cc07b2d299cdadafca8a
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-20 10:05:50 +0700

    auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,23 @@
-import { Router } from "express";
-const router = Router();
+import { Router } from 'express'
+const router = Router()
 
 // Define a GET route for fetching the list of products
-router.get("/authorizations", (req, res) => {
-  res.json("List of authorizations");
-});
+router.get('/authorizations', (req, res) => {
+  res.json('List of authorizations')
+})
 
 // Define a GET route for fetching a single user
-router.get("/signup", (req, res) => {
-  res.json("signup");
-});
+router.get('/signup', (req, res) => {
+  res.json('signup')
+})
 
-router.get("/login", (req, res) => {
-  res.json("login");
-});
+router.get('/login', (req, res) => {
+  res.json('login')
+})
 
-router.get("/logout", (req, res) => {
-  res.json("logout");
-});
+router.get('/logout', (req, res) => {
+  res.json('logout')
+})
 
 // Export the router
-export default router;
+export default router
