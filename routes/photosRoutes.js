1397c6ba0c99e535b0951e71232e9d8a33935123
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-20 12:37:39 +0700

    format

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,27 +1,27 @@
-import { Router } from "express";
-const router = Router();
+import { Router } from 'express'
+const router = Router()
 
 // Define a GET route for fetching the list of users
-router.get("/photos", (req, res) => {
+router.get('/photos', (req, res) => {
   res.json([
     {
-      photo_id: "1",
-      url: "https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      photo_id: '1',
+      url: 'https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-      photo_id: "2",
-      url: "https://images.pexels.com/photos/2098405/pexels-photo-2098405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-  ]);
-});
+      photo_id: '2',
+      url: 'https://images.pexels.com/photos/2098405/pexels-photo-2098405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    }
+  ])
+})
 
 // Define a GET route for fetching a single user
-router.get("/photos/1", (req, res) => {
+router.get('/photos/1', (req, res) => {
   res.json({
-    photo_id: "1",
-    url: "https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  });
-});
+    photo_id: '1',
+    url: 'https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  })
+})
 
 // Export the router
-export default router;
+export default router
