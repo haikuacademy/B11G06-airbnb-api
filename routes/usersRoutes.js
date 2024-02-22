9f63e23f6d7a1520b65f1d2d8cdab02ba0d4d88a
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-22 12:29:32 +0700

    router.post users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -16,11 +16,39 @@ router.get('/users', async (req, res) => {
 
 // Define a GET route for fetching a single user
 router.get('/users/:userId', async (req, res) => {
-  const user = await db.query(`SELECT * FROM users WHERE user_id = ${req.params.userId}`)
+  const user = await db.query(
+    `SELECT * FROM users WHERE user_id = ${req.params.userId}`
+  )
   // console.log(user)
   res.json(user.rows[0])
 })
 
+router.post('/users', async (req, res) => {
+  const { email, password, first_name, last_name, profile_pictureurl } =
+    req.body
+
+  console.log('req.body', {
+    email,
+    password,
+    first_name,
+    last_name,
+    profile_pictureurl
+  })
+
+  const queryString = `
+        INSERT INTO users (email, password, first_name, last_name, profile_pictureurl)
+        VALUES ('${email}', '${password}', '${first_name}', '${last_name}', '${profile_pictureurl}')
+        RETURNING user_id
+    `
+  try {
+    console.log(queryString)
+    const result = await db.query(queryString)
+    console.log(result)
+    res.send(result.rows[0])
+  } catch (e) {
+    res.send({ error: e.message })
+  }
+})
+
 // Export the router
 export default router
-
