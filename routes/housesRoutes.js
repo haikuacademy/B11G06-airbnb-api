7f6a4875f60992bb5342c88658df7f86900d0a32
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-22 12:38:03 +0700

    fix merge

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -5,34 +5,38 @@ import { clearConfigCache } from 'prettier'
 
 //HOUSE ID ROUTE
 router.get('/houses/:houseId', async (req, res) => {
-    try {
-        console.log('yes coming through')
-        const { rows } = await db.query(`SELECT * FROM houses WHERE house_id = ${req.params.houseId}`)
-        if (rows[0]) {
-            res.json(rows[0])
-        } else {
-            throw new Error('House not found')
-        }
-    } catch (err) {
-        console.error(err.message)
-        res.json({error: err.message })
+  try {
+    console.log('yes coming through')
+    const { rows } = await db.query(
+      `SELECT * FROM houses WHERE house_id = ${req.params.houseId}`
+    )
+    if (rows[0]) {
+      res.json(rows[0])
+    } else {
+      throw new Error('House not found')
     }
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
 })
 
-router.post('/houses', async (req, res) => { 
-    const { user_id, description, location, bedrooms, bathrooms, price_night } = req.body
-    console.log('req.body', { user_id, description })
-    const queryString = `
+router.post('/houses', async (req, res) => {
+  const { user_id, description, location, bedrooms, bathrooms, price_night } =
+    req.body
+  console.log('req.body', { user_id, description })
+  const queryString = `
         INSERT INTO houses (user_id, description, location, bedrooms, bathrooms, price_night)
         VALUES (${user_id}, '${description}', '${location}', ${bedrooms}, ${bathrooms}, ${price_night})
         RETURNING house_id
     `
-    try {
-        const result = await db.query(queryString)
-        res.send(result.rows[0])
-    } catch (e) {
-        res.send({error: e.message})
-    }
+  try {
+    const result = await db.query(queryString)
+    res.send(result.rows[0])
+  } catch (e) {
+    res.send({ error: e.message })
+  }
+})
 
 router.get('/houses', async (req, res) => {
   try {
