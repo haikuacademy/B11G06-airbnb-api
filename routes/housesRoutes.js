e4ca40d7542f84179687ea5918deeffdcdfc1c5a
Emiliano <onorati@gmail.com>
2024-02-22 11:40:40 +0700

    fix /houses/:houseId

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -15,12 +15,17 @@ router.get('/houses', async (req, res) => {
 
 router.get('/houses/:houseId', async (req, res) => {
     try {
-        const houseId = await db.query(`SELECT * FROM houses WHERE house_id = ${req.params.houseId}`)
-        res.json(houseId.rows[0])
+        console.log('yes coming through')
+        const { rows } = await db.query(`SELECT * FROM houses WHERE house_id = ${req.params.houseId}`)
+        if (rows[0]) {
+            res.json(rows[0])
+        } else {
+            throw new Error('House not found')
+        }
     } catch (err) {
         console.error(err.message)
-        res.json(err)
-        }
+        res.json({error: err.message })
+    }
 })
 
 export default router
